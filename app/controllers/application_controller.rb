5f077866a3ce736a1c1cc33a92fb220c1fe397d5
羽尻慎吾 <hajirishingo@hajirishingonoMacBook-Air.local>
2024-08-09 10:01:02 +0900

    AND検索とgooglemap導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,13 @@ class ApplicationController < ActionController::Base
 
   def set_search
     @q = Plan.ransack(params[:q])
-    @search_plans = @q.result(distinct: true).includes(:user).order(created_at: :desc)
+    # @search_plans = @q.result(distinct: true).includes(:user).order(created_at: :desc)
+    if params[:q].present? && params[:q][:title_or_body_or_spots_store_name_or_spots_address_cont].present?
+      key_words = params[:q][:title_or_body_or_spots_store_name_or_spots_address_cont].split(/[\p{blank}\s]+/)
+      grouping_hash = key_words.reduce({}) do |hash, word|
+        hash.merge(word => { title_or_body_or_spots_store_name_or_spots_address_cont: word })
+      end
+      @search_plans = Plan.ransack({ combinator: 'and', groupings: grouping_hash }).result.includes(:user).order(created_at: :desc)
+    end
   end
 end
