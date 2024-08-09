301789f044fa9bdeda0f006712f5a64dddae62d2
羽尻慎吾 <hajirishingo@hajirishingonoMacBook-Air.local>
2024-08-09 10:53:29 +0900

    検索機能の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
       grouping_hash = key_words.reduce({}) do |hash, word|
         hash.merge(word => { title_or_body_or_spots_store_name_or_spots_address_cont: word })
       end
-      @search_plans = Plan.ransack({ combinator: 'and', groupings: grouping_hash }).result.includes(:user).order(created_at: :desc)
+      @search_plans = Plan.ransack({ combinator: 'and', groupings: grouping_hash }).result(distinct: true).includes(:user).order(created_at: :desc)
     end
   end
 end
