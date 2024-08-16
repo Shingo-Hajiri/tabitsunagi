f39f79c342ea8160948fcf2479f9811bb5b35716
羽尻慎吾 <hajirishingo@hajirishingonoMacBook-Air.local>
2024-08-16 18:23:03 +0900

    本人確認スキップ

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -40,8 +40,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @profile.email.blank?
         email = @omniauth['info']['email'] ? @omniauth['info']['email'] : fake_email(@omniauth["uid"], @omniauth["provider"])
         @profile = current_user || User.create!(provider: @omniauth['provider'], uid: @omniauth['uid'], email: email, name: @omniauth['info']['name'], password: Devise.friendly_token[0, 20])
-        @profile.skip_confirmation!
       end
+      @profile.skip_confirmation!
       @profile.set_values(@omniauth)
       sign_in(:user, @profile)
 
