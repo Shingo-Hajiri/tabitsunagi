b58e4156a99edeb551e3afca1243c2bec8d31f6b
羽尻慎吾 <hajirishingo@hajirishingonoMacBook-Air.local>
2024-08-24 10:18:24 +0900

    サイトについて更新、投稿編集画面のフォーム修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/environment')
+
 rails_env = ENV['RAILS_ENV'] || :development
+
 set :environment, rails_env
 set :output, "#{Rails.root}/log/cron.log"
+
 every 1.day, at: '4:00 am' do
   rake '-s sitemap:refresh'
 end
