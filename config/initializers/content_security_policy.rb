99a57d6ddbd69099f2562e4195594d82b4af048e
羽尻慎吾 <hajirishingo@hajirishingonoMacBook-Air.local>
2024-09-09 23:42:55 +0900

    CSP_google設定

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -27,9 +27,9 @@ Rails.application.configure do
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
   end
-#   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src style-src)
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
+# Generate session nonces for permitted importmap, inline scripts, and inline styles.
+# config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+# config.content_security_policy_nonce_directives = %w(script-src style-src)
+# Report violations without enforcing the policy.
+# config.content_security_policy_report_only = true
 end
