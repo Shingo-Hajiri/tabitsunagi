41b8c951215ccedbdc8c2a9c18d3e31fde287c7d
羽尻慎吾 <hajirishingo@hajirishingonoMacBook-Air.local>
2024-09-10 10:35:48 +0900

    SEO対策(追加施策)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,10 +29,6 @@ Rails.application.configure do
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # 以下4つは追記した
-  config.assets.js_compressor = :uglifier
-  config.assets.digest = true
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
